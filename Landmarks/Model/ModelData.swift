1a2dcc7654ca3ea4e716385a2f5759807b917bd3
Danil Kaist <kaystd@gmail.com>
2021-08-21 14:39:57 +0700

    App design

---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,48 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Danil on 15.08.2021.
+//
+
+import Foundation
+import Combine
+
+final class ModelData: ObservableObject {
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
+    @Published var profile = Profile.default
+
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
+}
+
+func load<T: Decodable>(_ fileName: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: fileName, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(fileName) in main bundle.")
+    }
+
+    do {
+        try data = Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't find \(fileName) in main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(fileName) as \(T.self):\n\(error)")
+    }
+}
---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Danil on 20.08.2021.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+    static let `default` = Profile(username: "j_smith")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { self.rawValue }
+    }
+}
