c9a6ee42716f2921c5915bba26abe6c9e4fc8ece
Danil Kaist <kaystd@gmail.com>
2021-08-21 17:47:09 +0700

    WatchOS and MacOS integration

---
+++ MacLandmarks/LandmarkSettings.swift
@@ -0,0 +1,33 @@
+//
+//  LandmarkSettings.swift
+//  MacLandmarks
+//
+//  Created by Danil on 21.08.2021.
+//
+
+import SwiftUI
+
+struct LandmarkSettings: View {
+    @AppStorage("MapView.zoom")
+    private var zoom: MapView.Zoom = .medium
+
+    var body: some View {
+        Form {
+            Picker("Map Zoom:", selection: $zoom) {
+                ForEach(MapView.Zoom.allCases) { level in
+                    Text(level.rawValue)
+                }
+            }
+            .pickerStyle(InlinePickerStyle())
+        }
+        .frame(width: 300)
+        .navigationTitle("LandmarkSettings")
+        .padding(80)
+    }
+}
+
+struct LandmarkSettings_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkSettings()
+    }
+}
